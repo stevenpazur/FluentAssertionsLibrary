60c9b473c5f99452fa5701214f5bdc4c52850fad
Steven Pazur <sjpazur@yahoo.com>
2021-05-10 11:48:15 -0700

    compiled successfully

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+
+namespace FluentAssertionsLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class exampleClass {
+            public void additionMethod() {
+                var first = 3;
+                var second = 7;
+                var result = first + second;
+
+                result.Should().Be(10);
+            }
+        }
+    }
+}
